908e8ce11634a1d62c055709bd9552fb702f755f
Arnold Daniels <arnold@jasny.net>
2018-09-23 21:21:38 +0200

    Added cleanup and filter operation. FilterOperation is faster than using FilterIterator.

---
+++ tests/Operation/CleanupOperationTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Jasny\Iterator\Tests\Operation;
+
+use Jasny\IteratorProjection\Operation\CleanupOperation;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Jasny\IteratorProjection\Operation\CleanupOperation
+ * @covers \Jasny\IteratorProjection\Operation\AbstractOperation
+ */
+class CleanupOperationTest extends TestCase
+{
+    public function testIterate()
+    {
+        $values = ['one', 'two', null, 'foo', 0, '', null, [], -100];
+
+        $iterator = new CleanupOperation($values);
+
+        $result = iterator_to_array($iterator);
+
+        $expected = [0 => 'one', 1 => 'two', 3 => 'foo', 4 => 0, 5 => '', 7 => [], 8 => -100];
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIterateGenerator()
+    {
+        $keys = [(object)['a' => 'a'], ['b' => 'b'], null, 'd', 'd'];
+        $values = [1, 2, 3, 4, null];
+
+        $loop = function($keys, $values) {
+            foreach ($keys as $i => $key) {
+                yield $key => $values[$i];
+            }
+        };
+
+        $generator = $loop($keys, $values);
+        $iterator = new CleanupOperation($generator);
+
+        $resultKeys = [];
+        $resultValues = [];
+
+        foreach ($iterator as $key => $value) {
+            $resultKeys[] = $key;
+            $resultValues[] = $value;
+        }
+
+        $this->assertSame([1, 2, 3, 4], $resultValues);
+        $this->assertSame(array_slice($keys, 0, 4), $resultKeys);
+    }
+
+    public function testIterateEmpty()
+    {
+        $iterator = new CleanupOperation(new \EmptyIterator());
+
+        $result = iterator_to_array($iterator);
+
+        $this->assertSame([], $result);
+    }
+}
---
+++ tests/Operation/FilterOperationTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Jasny\Iterator\Tests\Operation;
+
+use Jasny\IteratorProjection\Operation\FilterOperation;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Jasny\IteratorProjection\Operation\FilterOperation
+ * @covers \Jasny\IteratorProjection\Operation\AbstractOperation
+ */
+class FilterOperationTest extends TestCase
+{
+    public function testIterate()
+    {
+        $values = range(-10, 10);
+
+        $iterator = new FilterOperation($values, function($value) {
+            return $value % 2 === 0;
+        });
+
+        $result = iterator_to_array($iterator);
+
+        $expectedKeys = range(0, 20, 2);
+        $expectedValues = range(-10, 10, 2);
+
+        $this->assertSame(array_combine($expectedKeys, $expectedValues), $result);
+    }
+
+    public function testIterateKey()
+    {
+        $values = ['apple' => 'green', 'berry' => 'blue', 'cherry' => 'red', 'apricot' => 'orange'];
+
+        $iterator = new FilterOperation($values, function($value, $key) {
+            return $key[0] === 'a';
+        });
+
+        $result = iterator_to_array($iterator);
+
+        $this->assertSame(['apple' => 'green', 'apricot' => 'orange'], $result);
+    }
+
+    public function testIterateGenerator()
+    {
+        $keys = [(object)['a' => 'a'], ['b' => 'b'], null, 'd', 'd'];
+
+        $loop = function($keys) {
+            foreach ($keys as $i => $key) {
+                yield $key => $i + 1;
+            }
+        };
+
+        $generator = $loop($keys);
+        $iterator = new FilterOperation($generator, function($value, $key) {
+            return !is_scalar($key);
+        });
+
+        $resultKeys = [];
+        $resultValues = [];
+
+        foreach ($iterator as $key => $value) {
+            $resultKeys[] = $key;
+            $resultValues[] = $value;
+        }
+
+        $this->assertSame([1, 2, 3], $resultValues);
+        $this->assertSame(array_slice($keys, 0, 3), $resultKeys);
+    }
+
+    public function testIterateEmpty()
+    {
+        $iterator = new FilterOperation(new \EmptyIterator(), function($value, $key) {
+            return true;
+        });
+
+        $result = iterator_to_array($iterator);
+
+        $this->assertSame([], $result);
+    }
+}
