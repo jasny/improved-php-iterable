9366b5f6b9147fd2ded1e5e8c127f84c6ce29e47
Arnold Daniels <arnold@jasny.net>
2018-09-25 18:21:56 +0200

    declare(string_types=1) for tests

--- tests/IterableFilterTest.php
+++ tests/IterableFilterTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jasny\Tests;
 
 use function Jasny\iterable_find;
--- tests/Iterator/CombineIteratorTest.php
+++ tests/Iterator/CombineIteratorTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jasny\Tests\Iterator;
 
 use Jasny\Iterator\CombineIterator;
