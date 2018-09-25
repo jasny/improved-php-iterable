e5242e0827cb6a452eff5233b31c084ec9fe6c5e
Arnold Daniels <arnold@jasny.net>
2018-09-25 06:28:42 +0200

    Added function iterable_slice

---
+++ src/iterable_slice.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jasny;
+
+/**
+ * Get a limited subset of the elements using an offset and (optionally) a limit.
+ *
+ * @param iterable $iterable
+ * @param int      $offset
+ * @param int|null $limit
+ * @return \Generator
+ */
+function iterable_slice(iterable $iterable, int $offset, ?int $limit = null): \Generator
+{
+    $counter = 0;
+    $end = isset($limit) ? ($offset + $limit) : PHP_INT_MAX;
+
+    foreach ($iterable as $key => $value) {
+        if ($counter === $end) {
+            return;
+        }
+
+        if ($counter >= $offset) {
+            yield $key => $value;
+        }
+
+        $counter++;
+    }
+}
---
+++ tests/IterableSliceTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Jasny\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Jasny\iterable_slice;
+
+/**
+ * @covers \Jasny\iterable_slice
+ */
+class IterableSliceTest extends TestCase
+{
+    use ProvideIterablesTrait;
+    use LazyExecutionIteratorTrait;
+
+    public function provider()
+    {
+        return $this->provideIterables(['one', 'two', 'three', 'four', 'five', 'six']);
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function test($values)
+    {
+        $iterator = iterable_slice($values, 2, 3);
+        $result = iterator_to_array($iterator, true);
+
+        $this->assertEquals([2 => 'three', 3 => 'four', 4 => 'five'], $result);
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testLimit($values)
+    {
+        $iterator = iterable_slice($values, 0, 3);
+        $result = iterator_to_array($iterator, true);
+
+        $this->assertEquals(['one', 'two', 'three'], $result);
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testOffset($values)
+    {
+        $iterator = iterable_slice($values, 2);
+        $result = iterator_to_array($iterator, true);
+
+        $this->assertEquals([2 => 'three', 3 => 'four', 4 => 'five', 5 => 'six'], $result);
+    }
+
+    public function testEmpty()
+    {
+        $iterator = iterable_slice(new \EmptyIterator(), 0, 1);
+        $result = iterator_to_array($iterator, true);
+
+        $this->assertEquals([], $result);
+    }
+}
