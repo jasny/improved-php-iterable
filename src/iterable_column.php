f722e47a86ac3ed6a1bb19344b4c94b198c6e8bf
Arnold Daniels <arnold@jasny.net>
2018-09-26 05:07:06 +0200

    Added iterable_column

---
+++ src/iterable_column.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jasny;
+
+/**
+ * Get a key and/or value of each element of the iterable.
+ *
+ * The elements need to be objects or arrays.
+ * For scalar elements or if the property/index doesn't exist, the value and/or key will be null.
+ *
+ * @param iterable $iterable
+ * @param mixed|null $valueColumn  Value property/index, null for complete value
+ * @param mixed|null $keyColumn    Key property/index, null to keep current keys
+ * @return \Generator
+ */
+function iterable_column(iterable $iterable, $valueColumn, $keyColumn = null): \Generator
+{
+    foreach ($iterable as $key => $value) {
+        if (is_array($value) || (is_object($value) && $value instanceof \ArrayAccess)) {
+            $key = isset($keyColumn) ? ($value[$keyColumn] ?? null) : $key;
+            $value = isset($valueColumn) ? ($value[$valueColumn] ?? null) : $value;
+        } elseif (is_object($value) && !$value instanceof \DateTimeInterface) {
+            $key = isset($keyColumn) ? ($value->$keyColumn ?? null) : $key;
+            $value = isset($valueColumn) ? ($value->$valueColumn ?? null) : $value;
+        } else {
+            $key = isset($keyColumn) ? null : $key;
+            $value = isset($valueColumn) ? null : $value;
+        }
+
+        yield $key => $value;
+    }
+}
